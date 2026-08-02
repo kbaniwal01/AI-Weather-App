f803f1744b4c893864a09401937516101ab81ae2
Kunal Baniwal <kbaniwal01@gmail.com>
2026-08-03 01:31:38 +0530

    Update API URL for production

--- src/components/services/weatherApi.js
+++ src/components/services/weatherApi.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:5001/api";
+const BASE_URL = "https://ai-weather-backend-ypjm.onrender.com";
 
 export async function getWeather(city) {
 
